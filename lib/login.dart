e52cfdde3a8a698fea689048cdea4a9704a0c0f5
Syed Khaja Fareeduddin <82967660+Fareeduddin5@users.noreply.github.com>
2023-03-05 12:32:58 +0530

    Add files via upload

--- lib/login.dart
+++ lib/login.dart
@@ -19,118 +19,123 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: const Padding(
-          padding: EdgeInsets.fromLTRB(20, 60, 20, 30),
-          child: Text(
-            'ADMIN PANEL',
-            style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                fontFamily: 'lato'),
-          ),
-        ),
-      ),
+      // appBar: AppBar(),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(1.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.4,
-                child: Image.asset('images/drone.png'),
+          padding: const EdgeInsets.only(top: 50.0),
+          child: Column(
+            children: [
+              const Text(
+                'ADMIN PANEL',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.3,
-                child: Column(
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    const Padding(
-                      padding: EdgeInsets.fromLTRB(10, 15, 20, 30),
-                      child: Text(
-                        textAlign: TextAlign.left,
-                        'WELCOME BACK!',
-                        style: TextStyle(
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      child: Image.asset('images/drone.jpg'),
                     ),
                     SizedBox(
-                        height: 100.0,
-                        child: Column(
-                          children: [
-                            const Text(
-                              'USERNAME',
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          const Padding(
+                            padding: EdgeInsets.fromLTRB(10, 15, 20, 30),
+                            child: Text(
                               textAlign: TextAlign.left,
+                              'WELCOME BACK!',
                               style: TextStyle(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.w500,
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.w900,
                               ),
                             ),
-                            TextField(
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          SizedBox(
+                            height: 100.0,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'USERNAME',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                TextField(
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 100.0,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'PASSWORD',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                TextField(
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              const Expanded(
+                                flex: 2,
+                                child: Text(
+                                  textAlign: TextAlign.left,
+                                  'FORGOT PASSWORD?',
+                                  style: TextStyle(
+                                      fontSize: 16.0, color: Colors.grey),
                                 ),
                               ),
-                            )
-                          ],
-                        )),
-                    SizedBox(
-                      height: 100.0,
-                      child: Column(
-                        children: [
-                          const Text(
-                            'PASSWORD',
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.0),
+                              Expanded(
+                                flex: 1,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: const Size(200, 40),
+                                    primary: const Color(0xFF24A19C),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                  child: const Text('Login'),
+                                ),
                               ),
-                            ),
-                          )
+                            ],
+                          ),
                         ],
                       ),
-                    ),
-                    Row(
-                      children: [
-                        const Expanded(
-                          flex: 2,
-                          child: Text(
-                            textAlign: TextAlign.left,
-                            'FORGOT PASSWORD?',
-                            style:
-                                TextStyle(fontSize: 16.0, color: Colors.grey),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              minimumSize: const Size(200, 40),
-                              primary: const Color(0xFF24A19C),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                            ),
-                            onPressed: () {},
-                            child: const Text('Login'),
-                          ),
-                        ),
-                      ],
                     ),
                   ],
                 ),
